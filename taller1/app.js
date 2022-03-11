4a23cc65cd579038ca63f91ee1d97d6af522e8a7
Pablo Marino <pmarino2013@gmail.com>
2022-03-11 15:24:05 -0300

    Taller de precios y descuentos terminado

--- taller1/app.js
+++ taller1/app.js
@@ -1,8 +1,15 @@
-let precioOriginal = 120;
-let descuento = 20;
+// let precioOriginal = 120;
+// let descuento = 20;
 
-let porcentajePrecioConDescuento = 100 - descuento;
-let precioConDescuento = precioOriginal * (porcentajePrecioConDescuento / 100);
+// let porcentajePrecioConDescuento = 100 - descuento;
+// let precioConDescuento = precioOriginal * (porcentajeDescuento / 100);
+
+function calcularPrecioConDescuento(precio, descuento) {
+  let porcentajePrecioConDescuento = 100 - descuento;
+  let precioConDescuento = precio * (porcentajePrecioConDescuento / 100);
+
+  return precioConDescuento;
+}
 
 console.log({
   precioOriginal,
@@ -11,4 +18,23 @@ console.log({
   precioConDescuento,
 });
 
-// funcion que tome los datos, realice el proceso y entregue el resultado
+//   Cuando se realiza el submit del formulario
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let precio = parseFloat(document.querySelector("#precio").value);
+  let descuento = parseFloat(document.querySelector("#descuento").value);
+
+  if (precio > 0 && descuento > 0) {
+    document.querySelector(
+      "#resultado"
+    ).value = `El producto con el descuento del ${descuento}% tiene un costo de $${calcularPrecioConDescuento(
+      precio,
+      descuento
+    )}`;
+  } else {
+    alert("Debe ingresar números válidos");
+  }
+});
+
+//Tarea
+// Nuestros clientes están muy felices de poder calcular el precio final de sus productos después del descuento, pero la tienda tiene una nueva solicitud para nosotros: implementar descuentos con cupones.
