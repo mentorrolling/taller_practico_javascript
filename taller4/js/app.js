291fc7d09f1274810b2512a7ff6a88020c72532a
Pablo Marino <pmarino2013@gmail.com>
2022-03-25 15:32:33 -0300

    Taller n° 4 terminado, inicio de taller 5

--- taller4/js/app.js
+++ taller4/js/app.js
@@ -1,3 +1,5 @@
+let heroes = {};
+
 const getDatosHeroes = async () => {
   try {
     const response = await fetch("../data/superheroes.json");
@@ -11,4 +13,58 @@ const getDatosHeroes = async () => {
 
 getDatosHeroes().then((respuesta) => {
   console.log(respuesta);
+  heroes = { ...respuesta };
+  obtenerDatos();
 });
+
+// let num=[1,2,3]
+
+// const [num1, num2, num3]=num
+
+const recorrerPoderes = (array) => {
+  let lista = "";
+  for (let index = 0; index < array.length; index++) {
+    lista += `<li>${array[index]}</li>`;
+  }
+
+  return lista;
+};
+
+const obtenerDatos = () => {
+  const { squadName, homeTown, formed, members } = heroes;
+  //obtener los datos del titulo, subtitulo
+  //luego tenemos que mostrarlos
+
+  document.querySelector("#title").innerText = `${squadName}`;
+  document.querySelector(
+    "#subtitle"
+  ).innerHTML = `<h3><b>HomeTown:</b> ${homeTown} | <b>Formed:</b> ${formed}</h3>`;
+
+  //obtener los miembros y mostrar los datos:
+  //nombre, identidad secreta, edad, poderes
+
+  members.forEach((heroe) => {
+    const { name, secretIdentity, age, powers } = heroe;
+    console.log(powers);
+    let col = document.createElement("div");
+    col.classList = "col";
+
+    let contenido = `<h2>${name}</h2> 
+    <p><b>Secret Identity:</b> ${secretIdentity}</p>
+    <p><b>Age:</b> ${age}</p>
+    <p><b>Superpowers:</b></p>
+    <ul>
+      ${recorrerPoderes(powers)}
+    </ul>
+    `;
+
+    col.innerHTML = contenido;
+    document.querySelector("#container").appendChild(col);
+  });
+
+  //mostrar en una lista los poderes de cada uno
+
+  //recorrer el arreglo de heroes
+  //por cada heroe debo generar una lista con sus poderes
+  //powers[]
+};
---
+++ taller5/helpers/fetch.js
@@ -0,0 +1,5 @@
+let url = "https://fakestoreapi.com/products";
+
+const getProductos = async () => {
+  //fetch al endpoint
+};
